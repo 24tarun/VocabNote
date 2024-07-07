ea603dac649610536a4d85fab4592c44c2069d22
tarunhariharan <tarun2k01@gmail.com>
2024-07-07 17:02:06 +0200

    routine

--- firstapp/views.py
+++ firstapp/views.py
@@ -5,7 +5,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 # Path to your credentials JSON file
-CREDS_FILE = 'path/to/your/credentials.json'
+CREDS_FILE = r'D:\coding\credentials\vocabnote-425819-9e3b675a3e5a'
 
 # Define the scope
 SCOPE = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
